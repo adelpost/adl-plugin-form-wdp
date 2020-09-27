1a1077e2d2ec43bd4090e9656a426916a8c9de41
adelpost <fjtgwebs@gmail.com>
2020-09-27 15:34:20 +0200

    Terminada la primera prueba

--- kfp-plugin-form.php
+++ kfp-plugin-form.php
@@ -37,15 +37,16 @@ add_shortcode('kfp_plugin_form', 'KFP_Plugin_form');
 function KFP_Plugin_form()
 {
     global $wpdb;
-   //Inserccion de datos
-    if(!empty($_POST) 
-    AND $_POST['nombre'] != '' 
-    AND is_email($_POST['correo'])
-    AND $_POST['nivel_html'] != '' 
-    AND $_POST['nivel_css'] != '' 
-    AND $_POST['nivel_js'] != '' 
-    AND $_POST['aceptacion'] == '1'
-    ){
+    //Inserccion de datos
+    if (
+        !empty($_POST)
+        and $_POST['nombre'] != ''
+        and is_email($_POST['correo'])
+        and $_POST['nivel_html'] != ''
+        and $_POST['nivel_css'] != ''
+        and $_POST['nivel_js'] != ''
+        and $_POST['aceptacion'] == '1'
+    ) {
         $table_aspirante = $wpdb->prefix . 'aspirante';
         $nombre = sanitize_text_field($_POST['nombre']);
         $correo = sanitize_email($_POST['correo']);
@@ -55,71 +56,117 @@ function KFP_Plugin_form()
         $aceptacion = (int)$_POST['aceptacion'];
         $create_at = date('Y-m-d H:i:s');
         $wpdb->insert(
-            $table_aspirante, 
+            $table_aspirante,
             array(
-            'nombre'=> $nombre,
-            'correo'=> $correo,
-            'nivel_html'=> $nivel_html,
-            'nivel_css'=> $nivel_css,
-            'nivel_js'=> $nivel_js,
-            'aceptacion'=> $aceptacion,
-            'create_at'=> $create_at,
-        )
-    );
-    } 
-
+                'nombre' => $nombre,
+                'correo' => $correo,
+                'nivel_html' => $nivel_html,
+                'nivel_css' => $nivel_css,
+                'nivel_js' => $nivel_js,
+                'aceptacion' => $aceptacion,
+                'create_at' => $create_at,
+            )
+        );
+        echo "<p class='exito'><b>Tus datos han sido registrados</b>. Gracias
+            por tu interés. En breve contactaré contigo.<p>";
+    }
+    wp_enqueue_style('css_aspirante', plugins_url('style.css', __FILE__));
     ob_start();
 ?>
-     <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante" class="cuestionario"">
-    <div class="form-input">
+    <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante" class="cuestionario"">
+    <div class=" form-input">
+        <?php wp_nonce_field('graba_aspirante', 'aspirante_nonce');
+        ?>
         <label for="nombre">Nombre</label>
         <input type="text" name="nombre" id="nombre" required>
-    </div>
-    <div class="form-input">
-        <label for='correo'>Correo</label>
-        <input type="email" name="correo" id="correo" required>
-    </div>
-    <div class="form-input">
-        <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
-        <input type="radio" name="nivel_html" value="1" required> Nada
-        <br><input type="radio" name="nivel_html" value="2" required> Estoy
-        aprendiendo
-        <br><input type="radio" name="nivel_html" value="3" required> Tengo
-        experiencia
-        <br><input type="radio" name="nivel_html" value="4" required> Lo
-        domino al dedillo
-    </div>
-    <div class="form-input">
-        <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
-        <input type="radio" name="nivel_css" value="1" required> Nada
-        <br><input type="radio" name="nivel_css" value="2" required> Estoy
-        aprendiendo
-        <br><input type="radio" name="nivel_css" value="3" required> Tengo
-        experiencia
-        <br><input type="radio" name="nivel_css" value="4" required> Lo
-        domino al dedillo
-    </div>
-    <div class="form-input">
-        <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
-        <input type="radio" name="nivel_js" value="1" required> Nada
-        <br><input type="radio" name="nivel_js" value="2" required> Estoy
-        aprendiendo
-        <br><input type="radio" name="nivel_js" value="3" required> Tengo
-        experiencia
-        <br><input type="radio" name="nivel_js" value="4" required> Lo domino
-        al dedillo
-    </div>
-    <div class="form-input">
-        <label for="aceptacion">La información facilitada se tratará
-            con respeto y admiración.</label>
-        <input type="checkbox" id="aceptacion" name="aceptacion" value="1" required> Entiendo y acepto las condiciones
-    </div>
-    <div class="form-input">
-        <input type="submit" value="Enviar">
-    </div>
+        </div>
+        <div class="form-input">
+            <label for='correo'>Correo</label>
+            <input type="email" name="correo" id="correo" required>
+        </div>
+        <div class="form-input">
+            <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
+            <input type="radio" name="nivel_html" value="1" required> Nada
+            <br><input type="radio" name="nivel_html" value="2" required> Estoy
+            aprendiendo
+            <br><input type="radio" name="nivel_html" value="3" required> Tengo
+            experiencia
+            <br><input type="radio" name="nivel_html" value="4" required> Lo
+            domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
+            <input type="radio" name="nivel_css" value="1" required> Nada
+            <br><input type="radio" name="nivel_css" value="2" required> Estoy
+            aprendiendo
+            <br><input type="radio" name="nivel_css" value="3" required> Tengo
+            experiencia
+            <br><input type="radio" name="nivel_css" value="4" required> Lo
+            domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
+            <input type="radio" name="nivel_js" value="1" required> Nada
+            <br><input type="radio" name="nivel_js" value="2" required> Estoy
+            aprendiendo
+            <br><input type="radio" name="nivel_js" value="3" required> Tengo
+            experiencia
+            <br><input type="radio" name="nivel_js" value="4" required> Lo domino
+            al dedillo
+        </div>
+        <div class="form-input">
+            <label for="aceptacion">La información facilitada se tratará
+                con respeto y admiración.</label>
+            <input type="checkbox" id="aceptacion" name="aceptacion" value="1" required> Entiendo y acepto las condiciones
+        </div>
+        <div class="form-input">
+            <input type="submit" value="Enviar">
+        </div>
     </form>
 <?php
     return ob_get_clean();
 }
 
+add_action("admin_menu", "kfp_Aspirante_menu");
 
+/**
+ * Agrega el menú del plugin al formulario de Wordpress
+ * 
+ * @return void
+ */
+
+function Kfp_Aspirante_menu()
+{
+    add_menu_page(
+        "Formulario Aspirante",
+        "Aspirantes",
+        "manage_options",
+        "kfp_aspirante_menu",
+        "Kfp_Aspirante_admin",
+        "dashicons-feedback",
+        75
+    );
+}
+
+function Kfp_Aspirante_admin()
+{
+    global $wpdb;
+    $table_aspirante = $wpdb->prefix . 'aspirante';
+    $aspirantes = $wpdb->get_results("SELECT * FROM $table_aspirante");
+    echo '<div class="wrap"><h1>Lista de aspirantes</h1>';
+    echo '<table class="wp-list-table widefat dixed striped">';
+    echo '<thead><tr><th width="30%">Nombre</th><th width="20%">Correo</th>';
+    echo '<th>HTML</th><th>CSS</th><th>JS</th><th>Total</th>';
+    echo '</tr></thead>';
+    echo '<tbody id="the-list">'; 
+    foreach($aspirantes as $aspirante){
+        $nombre= esc_textarea ( $aspirante->nombre );
+        $correo= esc_textarea ( $aspirante->correo );
+        $html= (int) $aspirante->nivel_html;
+        $css= (int) $aspirante->nivel_css;
+        $js= (int) $aspirante->nivel_js;
+        $total = $html + $css + $js;
+        echo "<tr><td>$nombre</td><td>$correo</td><td>$html</td><td>$css</td><td>$js</td><td>$total</td></tr>";
+    }
+    echo '</tbody></table>';
+}
