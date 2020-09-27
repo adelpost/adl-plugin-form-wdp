faa13c32f7426c642faae95fdb21e8de16d8c339
adelpost <fjtgwebs@gmail.com>
2020-09-27 13:08:58 +0200

    Crea ala tabla y guarda los datos del formulario

--- kfp-plugin-form.php
+++ kfp-plugin-form.php
@@ -6,14 +6,71 @@
  * Description: Plugin para crear un formulario personalizado. Utiliza el shotcode
  * shortcode [kfp_plugin_form]
  */
+
+register_activation_hook(__FILE__, 'Kfp_Aspirante_init');
+
+function Kfp_Aspirante_init()
+{
+
+    global $wpdb;
+    $table_aspirante = $wpdb->prefix . 'aspirante';
+    $charset_collate = $wpdb->get_charset_collate();
+    // prepara la consulta que vamos a lanzar para crear la tabla
+    $query = "CREATE TABLE IF NOT EXISTS $table_aspirante(
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        nombre varchar(40) NOT NULL,
+        correo varchar(100) NOT NULL,
+        nivel_html smallint(4) NOT NULL,
+        nivel_css smallint(4) NOT NULL,
+        nivel_js smallint(4) NOT NULL,
+        aceptacion smallint(4) NOT NULL,
+        create_at datetime NOT NULL,
+        UNIQUE (id)
+    ) $charset_collate";
+    include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta($query);
+}
+
 // Define el shortcode que pinta el formulario
 add_shortcode('kfp_plugin_form', 'KFP_Plugin_form');
 
 function KFP_Plugin_form()
 {
+    global $wpdb;
+   //Inserccion de datos
+    if(!empty($_POST) 
+    AND $_POST['nombre'] != '' 
+    AND is_email($_POST['correo'])
+    AND $_POST['nivel_html'] != '' 
+    AND $_POST['nivel_css'] != '' 
+    AND $_POST['nivel_js'] != '' 
+    AND $_POST['aceptacion'] == '1'
+    ){
+        $table_aspirante = $wpdb->prefix . 'aspirante';
+        $nombre = sanitize_text_field($_POST['nombre']);
+        $correo = sanitize_email($_POST['correo']);
+        $nivel_html = (int)$_POST['nivel_html'];
+        $nivel_css = (int)$_POST['nivel_css'];
+        $nivel_js = (int)$_POST['nivel_js'];
+        $aceptacion = (int)$_POST['aceptacion'];
+        $create_at = date('Y-m-d H:i:s');
+        $wpdb->insert(
+            $table_aspirante, 
+            array(
+            'nombre'=> $nombre,
+            'correo'=> $correo,
+            'nivel_html'=> $nivel_html,
+            'nivel_css'=> $nivel_css,
+            'nivel_js'=> $nivel_js,
+            'aceptacion'=> $aceptacion,
+            'create_at'=> $create_at,
+        )
+    );
+    } 
+
     ob_start();
 ?>
-     <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante class="cuestionario"">
+     <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante" class="cuestionario"">
     <div class="form-input">
         <label for="nombre">Nombre</label>
         <input type="text" name="nombre" id="nombre" required>
@@ -64,3 +121,5 @@ function KFP_Plugin_form()
 <?php
     return ob_get_clean();
 }
+
+
